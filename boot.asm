9af65f8c57a89eb4b1acdc371d5aaabae0febf36
pro-utkarshM <projects.utkarshMaurya@gmail.com>
2024-01-27 01:20:42 +0530

    Before asking chatGPT

--- boot.asm
+++ boot.asm
@@ -2,11 +2,29 @@ ORG 0x7C00
 BITS 16
 
 start:
+    mov si,message
+    call print
+    
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+
+print_char:
     mov ah, 0ah
-    mov al, 'A'
     int 0x10
 
-    jmp $
+message: db 'Hello World', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
